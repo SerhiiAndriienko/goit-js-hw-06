39a94c603897bd391458ac325490b4db86d3b9fb
serhiiandriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2023-03-05 16:28:02 +0100

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const countCategories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${countCategories.length}\n `);
+
+const categoriesEL = document.querySelectorAll("#categories .item");
+console.log(categoriesEL);
+categoriesEL.forEach((categoryEL) => {
+  const countAnimals = categoryEL.lastElementChild;
+  const categoriesTitle = categoryEL.firstElementChild;
+  console.log(`Category: ${categoriesTitle.textContent}`);
+  console.log(`Elements: ${countAnimals.children.length}\n `);
+});
